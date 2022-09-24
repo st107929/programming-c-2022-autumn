df9819b854beb955455c89965371555459b5d764
st107929 <113843555+st107929@users.noreply.github.com>
2022-09-25 01:52:51 +0300

    Add files via upload

---
+++ Task 7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	std::cin >> a;
+	std::cin >> b;
+
+	c = a;
+	a = b;
+	b = c;
+
+	std::cout << a << ", " << b << std::endl;
+	return EXIT_SUCCESS;
+}
