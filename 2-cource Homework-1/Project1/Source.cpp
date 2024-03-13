76635a38b27b4ee3adb954fd1cd9513ebf80e774
Artem <artem_2005a@mail.ru>
2024-03-13 21:55:58 +0300

    Code is changed

--- 2-cource Homework-1/Project1/Source.cpp
+++ 2-cource Homework-1/Project1/Source.cpp
@@ -1,42 +1,27 @@
 #include <iostream>
 
-
 class B
 {
 public:
-	char e[42]{}; // 42 bytes  
-	// 0 -> 41
-	bool h, g;// 1 * 2 bytes 
-	// 42 -> 43
-	int a; // 4 bytes 
-	// 44 -> 47
-	double c, d;  // 8 * 2 bytes
-	// 48 -> 63
-	int f[2]{ 0 };// 2 * 4 bytes
-	// 64 -> 71
-	int b; // 4 bytes
-	// 72 -> 76
+	char e[42]{}; 
+	bool h, g; 
+	int a;
+	double c, d;
+	int f[2]{ 0 };
+	int b; 
 };
 
 class Bmax
 {
 public:
-	bool h; // 1 byte
-	// 0 -> 1
+	bool h;
 	double c;
-	 //8 - > 15
-	bool g; // 1 byte
-	 //16 -> 17
-	double d;  // 8 bytes
-	// 24 -> 31
-	int a; // 4 bytes 
-	// 32 -> 35
-	char e[42]{}; // 42 bytes 
-	// 36 -> 77
-	int f[2]{ 0 }; // 2 * 4 bytes
-	// 80 -> 87
-	int b; // 4 bytes
-	// 88 -> 92
+	bool g;
+	double d;
+	int a;
+	char e[42]{};
+	int f[2]{ 0 };
+	int b;
 };
 //The number of memory cells should be a multiple of the most voluminous data type. 
 //In these classes it is double (8 byte), so the sum will be 76 + 4 = 80 bytes in class B, and 92 + 4 = 96 bytes in class Bmax 
